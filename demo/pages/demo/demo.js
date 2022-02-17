267638d633296383140ea8b34ddfa04e000bf427
wangls <wangls@sh.woowis.com>
2022-02-17 10:13:20 +0800

    2022Video

--- demo/pages/demo/demo.js
+++ demo/pages/demo/demo.js
@@ -1,5 +1,72 @@
 // pages/demo.js
+function getRandomColor() {
+  const rgb = []
+  for (let i = 0; i < 3; ++i) {
+    let color = Math.floor(Math.random() * 256).toString(16)
+    color = color.length === 1 ? '0' + color : color
+    rgb.push(color)
+  }
+  return '#' + rgb.join('')
+}
+
+
 Page({
+
+  inputValue: '',
+  data: {
+    src: '',
+    danmuList: [{
+      text: '第 1s 出现的弹幕',
+      color: '#ff0000',
+      time: 1
+    }, {
+      text: '第 3s 出现的弹幕',
+      color: '#ff00ff',
+      time: 3
+    }],
+  },
+
+  bindInputBlur(e) {
+    this.inputValue = e.detail.value
+  },
+
+  bindButtonTap() {
+    const that = this
+    wx.chooseVideo({
+      sourceType: ['album', 'camera'],
+      maxDuration: 60,
+      camera: ['front', 'back'],
+      success(res) {
+        that.setData({
+          src: res.tempFilePath
+        })
+      }
+    })
+  },
+
+  bindVideoEnterPictureInPicture() {
+    console.log('进入小窗模式')
+  },
+
+  bindVideoLeavePictureInPicture() {
+    console.log('退出小窗模式')
+  },
+
+  bindPlayVideo() {
+    console.log('1')
+    this.videoContext.play()
+  },
+  bindSendDanmu() {
+    this.videoContext.sendDanmu({
+      text: this.inputValue,
+      color: getRandomColor()
+    })
+  },
+
+  videoErrorCallback(e) {
+    console.log('视频错误信息:')
+    console.log(e.detail.errMsg)
+  },
 
   /**
    * 页面的初始数据
@@ -19,7 +86,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    this.videoContext = wx.createVideoContext('myVideo')
   },
 
   /**
@@ -61,6 +128,10 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    return {
+      title: 'video',
+      path: 'page/component/pages/video/video'
+    }
   }
+
 })
